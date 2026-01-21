3b8e428d1d02f217dc7c81304ff9056faf26de5c
Adrien Vacher <advacher@z2r8p4.42lyon.fr>
2026-01-21 18:37:40 +0100

    Optimized code structure and added prints directly within the class.

--- ex4/ft_garden_security.py
+++ ex4/ft_garden_security.py
@@ -2,16 +2,18 @@
 
 class Plant:
     def __init__(self, name: str, height: int, age: int):
-        self.name = name
+        print("Plant created:", name.capitalize())
+        self.name = name.capitalize()
         self.set_height(height)
         self.set_age(age)
 
     def set_height(self, h):
         if h < 0:
             print(f"Invalid operation attempted: height {h}cm [REJECTED]")
-            print("Security: Negative height rejected")
+            print("Security: Negative height rejected\n")
         else:
             self.__h = h
+            print(f"Height updated: {h}cm [ok]")
 
     def get_height(self):
         return self.__h
@@ -19,9 +21,10 @@ class Plant:
     def set_age(self, age):
         if age < 0:
             print(f"Invalid operation attempted: age {age}days [REJECTED]")
-            print("Security: Negative age rejected")
+            print("Security: Negative age rejected\n")
         else:
             self.__age = age
+        print(f"Age updated: {age} days [ok]\n")
 
     def get_age(self):
         return self.__age
@@ -29,6 +32,6 @@ class Plant:
 
 if __name__ == "__main__":
     print("=== Garden Security System ===")
-    rose = Plant("Rose", 10, 5)
-    print("Plant created:", rose.name)
-    print(f"Height updated: {rose.get_height()}cm [ok]")
+    r = Plant("rose", 25, 30)
+    r.set_height(-10)
+    print(f"Current plant: {r.name} ({r.get_height()}cm, {r.get_age()} days)")
